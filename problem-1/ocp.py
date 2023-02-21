f53a61dd6030213764442fe99f46b661e60b3426
Michael Barbosa <michaelfeliphe@gmail.com>
2023-02-20 22:03:14 -0300

    Read data file in OCP

--- problem-1/ocp.py
+++ problem-1/ocp.py
@@ -1,5 +1,22 @@
 # Imports
 import casadi as ca
+import numpy as np
+from math import asinh, exp, log
+from scipy.special import lambertw
+from scipy.io import loadmat
+
+# Read irradiation and demand data from file
+mat_contents = loadmat('vetores_sol_carga.mat')
+
+ini = 24*60
+fim = 2*24*60
+
+sol_real = mat_contents['sol_real'][ini:fim]
+fdemanda = -0.18+mat_contents['carga_real'][ini:fim]
+t_file = np.arange(1,len(sol_real)+1,1)
+
+Irradiation = ca.interpolant("Irradiation", "bspline", [t_file], sol_real.flatten())
+HydrogenDemand = ca.interpolant("HydrogenDemand", "bspline", [t_file], fdemanda.flatten())
 
 # Declare constants
 R = 8.314 # Gas constant
@@ -8,20 +25,14 @@ Q = 1.6e-19 # Elementary charge
 K = 1.38e-23 # Boltzmann constant
 
 # Declare electrolyzer parameters
-A_el = 212.5        # Stack area
-N_el = 1            # Number of cells
-P_h2 = 6.9          # Hydrogen partial pressure
-P_o2 = 1.3          # Oxygen partial pressure
-I_ao = 1.0631e-6    # Anode current density 
-I_co = 1e-3         # Cathode current density
-delta_b = 178.5     # Membrane thickness
-lambda_b = 21       # Membrana water content
-C_el = 0.5          # Thermal capacitance
-t_el = 298          # Temperature
-t_ab = 298          # Temperature 
-tau_el = 0          # NÃO ACHEI NO ARTIGO !!!
-P_el = 0            # NÃO ACHEI NO ARTIGO !!!
-R_I = 0             # NÃO ACHEI NO ARTIGO !!!
+N_c = 120        # Number of cells
+T = 298          # Temperature cell
+delta = 100e-6   # Thickness of membrane (tirei do google)
+lambda_ = 17     # degree of humidification of the membrane
+alpha_A = 2      # 
+i_0A = 1e-10     # 
+alpha_C = .5     # 
+i_0C = 1e-10     # 
 
 # Declare photovoltaic parameters
 N_ps = 8            # Number of panels in parallel
@@ -37,7 +48,6 @@ A = 1.6             # Factor. cell deviation from de ideal pn junction
 # Declare variables
 m_h2 = ca.SX.sym('m_h2') # State - Mass of hydrogen
 f_h2 = ca.SX.sym('f_h2') # Algebric - Produced hydrogen in the electrolyzer
-d_h2 = ca.SX.sym('d_h2') # Algebric - Hydrogen demand
 i_ps = ca.SX.sym('i_ps') # Algebric - Electrical current in photovoltaic panel
 v_ps = ca.SX.sym('v_ps') # Algebric - Voltage of photovoltaic panel
 v_el = ca.SX.sym('v_el') # Algebric - Voltage of electrolyzer
@@ -47,29 +57,30 @@ z = ca.vertcat(f_h2, v_el, v_ps, i_ps)  # Algebraic vector
 u = ca.vertcat(i_el)                    # Control vector
 
 # Intermediate electrolyzer variables
-v_el_0 = 1.23 - 0.0009*(t_el-298) + 2.3*R*t_el*ca.log(P_h2**2*P_o2)/(4*F) # Reversible potential of the electrolyzer
-ro_b = (0.005139*lambda_b - 0.00326) * ca.exp(1268*(1/303 - 1/t_el)) # Membrane conductivity
-v_etd = (R*t_el/F)*ca.sinh(.5*i_el/(I_ao))**-1 + (R*t_el/F)*ca.sinh(.5*i_el/(I_co))**-1 + i_el*delta_b/ro_b + R_I*i_el # Eletrode overpotential
-v_el_hom_ion = delta_b*i_el/(A_el*ro_b) # Ohmic overvoltage and ionic overpotential
+E_cell = 1.51184 - 1.5421e-3*T + 9.523e-5*T*log(T) + 9.84e-8*T**2
+V_actA = asinh(i_el/(2* i_0A))*R*T/(alpha_A*F)
+V_actC = asinh(i_el/(2* i_0C))*R*T/(alpha_C*F)
+ro = (0.005139*lambda_-0.00326)**(1268*(303**-1 - T**-1))
+R_cell = delta/ro;
 
 # Intermediate photovoltaic variables
-V_t = K*T_ps/Q;
-Iph = (Isc+Kl*(T-Tr))*lambda_;
-Irs = Ior*(T_ps/Tr)** 3*ca.exp(Q*Ego*(1/Tr-1/T_ps)/(K*A));	
+Vt = K*T_ps/Q
+Iph = (Isc+Kl*(T-Tr))*Irradiation(time)
+Irs = Ior*(T_ps/Tr)** 3*ca.exp(Q*Ego*(1/Tr-1/T_ps)/(K*A))
 
 # Diferential equations
-m_h2_dot = f_h2 - d_h2
+m_h2_dot = f_h2 - HydrogenDemand(time)
 f_x = ca.vertcat(m_h2_dot)
 
 # Algebraic equations
-f_h2 = N_el*i_el/F # Produced hydrogen in the electrolyzer
-v_el = v_el_0 + v_etd + v_el_hom_ion # Voltage of electrolyzer
-v_ps = 0 # Voltage of photovoltaic panel
-i_ps = N_ps*(Iph-Irs*(ca.exp(v_ps/(N_ss*Vt))-1)) # Current in photovoltaic panel
-f_z = ca.vertcat(f_h2, v_el)
+f_h2 = N_c*i_el/F 
+v_el = (E_cell + V_actC + V_actA + i_el*R_cell)
+v_ps = (N_ss*Vt*A*(lambertw(exp(1)*(Iph/Irs+1))-1)).real
+i_ps = N_ps*(Iph-Irs*(ca.exp(v_ps/(N_ss*Vt))-1)) 
+f_z = ca.vertcat(f_h2, v_el, v_ps, i_ps)
 
 # Lagrange cost function
-f_q = (v_el*i_el - v_ps*i_ps)**2
+f_q = (N_c**2*(v_el*i_el) - v_ps*i_ps)**2
 
 # Create an integrator
 dae = {'x':x, 'z':z, 'p':u, 'ode':f_x, 'alg':f_z, 'quad':f_q}
