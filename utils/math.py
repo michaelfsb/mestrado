c8d93a074ffcd055cf6c64b22175ad48c941c1eb
Michael Barbosa <michaelfeliphe@gmail.com>
2023-03-07 18:57:21 -0300

    Remove utils.math

--- utils/math.py
+++ utils/math.py
@@ -1,16 +1,2 @@
 import casadi as ca
 
-def lambertw(x):
-    """ 
-    Lambert W function analytic approximation
-    x >= 3e-5 -> error < 0.2%
-
-    Args:
-        x (float | ca.DM | ca.SX | ca.MX )
-        
-
-    Returns:
-        (float | ca.DM | ca.SX | ca.MX )
-    """    
-    E = 0.4586887
-    return (1+E)*ca.log(6/5*x/ca.log(12/5*x/ca.log(1+12/5*x)) ) -E*ca.log(2*x/ca.log(1+2*x))
