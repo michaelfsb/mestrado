529da0d13cddb1429362c8d1d7d344e415e4c7a6
Michael Barbosa <michaelfeliphe@gmail.com>
2023-03-07 23:15:45 -0300

    fdemanda: Nm3/h to Nm3/min

--- ocp-2-trapezoid-collocation.py
+++ ocp-2-trapezoid-collocation.py
@@ -31,7 +31,7 @@ time = ca.MX.sym('time') # Time
 [f_h2, v_el] = electrolyzer_model(i_el) # Hydrogen production rate (Nm3/min) and eletrolyzer voltage (V)
 f_h2 = p_el*f_h2 # Switching between electrolyzer states
 [i_ps, v_ps] = pv_model(Irradiation(time)) # Power and voltage of the photovoltaic panel (A, V)
-v_h2_dot = thank_model(f_h2, HydrogenDemand(time)/60) # Hydrongen volume rate in the tank (Nm3/min)
+v_h2_dot = thank_model(f_h2, HydrogenDemand(time)) # Hydrongen volume rate in the tank (Nm3/min)
 
 # Lagrange cost function
 f_l = (p_el*(I_e_min-i_el))*((N_el*v_el*i_el) - v_ps*i_ps)**2
