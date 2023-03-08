25305b60ef0a86fd5cb0d0d5324fdb90a3bb0337
Michael Barbosa <michaelfeliphe@gmail.com>
2023-03-07 23:08:38 -0300

    Refactoring ocp-2-trapezoid-collocation

--- ocp-2-trapezoid-collocation.py
+++ ocp-2-trapezoid-collocation.py
@@ -4,80 +4,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import interpolate
 
-from utils.math import lambertw
+from models.photovoltaic_panel import pv_model
+from models.electrolyzer import N_el, electrolyzer_model
+from models.tank import thank_model
 from models.input_data import Irradiation, HydrogenDemand
 
 # Preliminaries
-Tf = 1440 # Final time (min)
-#N = 2*Tf # Number of control intervals 
-N = 30
-M_0 = 0.65 # Initial mass of hydrogen (Nm3)
-M_min = 0.6 # Minimum mass of hydrogen (Nm3)
-M_max = 1 # Maximum mass of hydrogen (Nm3)
-I_e_0 = 30 # Initial current (A)
-I_e_std = 5 # Standby current (A)
-I_e_min = 25 # Minimum current (A)
-I_e_max = 100 # Maximum current (A)
+Tf = 1440   # Final time (min)
+N = 90      # Number of control intervals
 
-# Declare constants
-R = 8.314 # Gas constant
-F = 96485 # Faraday constant
-Q = 1.6e-19 # Elementary charge
-K = 1.38e-23 # Boltzmann constant
-
-# Declare electrolyzer parameters
-A_el = 212.5            # Stack area
-N_el = 22500            # Number of cells
-P_h2 = 6.9              # Hydrogen partial pressure
-P_o2 = 1.3              # Oxygen partial pressure
-I_ao = 1.0631e-6        # Anode current density 
-I_co = 1e-3             # Cathode current density
-delta_b = 178e-6        # Membrane thickness
-lambda_b = 21           # Membrana water content
-t_el = 298              # Temperature
-
-# Declare photovoltaic parameters
-N_ps = 8            # Number of panels in parallel
-N_ss = 300          # Number of panels in series
-T_ps =  298         # Temperature
-Tr = 298            # Reference temperature
-Isc = 3.27          # Short circuit current at Tr
-Kl = 0.0017         # Short circuit current temperature coeff
-Ior = 2.0793e-6     # Ior - Irs at Tr
-Ego = 1.1           # Band gap energy of the semiconductor
-A = 1.6             # Factor. cell deviation from de ideal pn junction
+M_0 = 0.65      # Initial volume of hydrogen (Nm3)
+M_min = 0.6     # Minimum volume of hydrogen (Nm3)
+M_max = 1       # Maximum volume of hydrogen (Nm3)
+I_e_0 = 30      # Initial current (A)
+I_e_min = 1     # Minimum current (A)
+I_e_std = 5     # Standby current (A)
+I_e_max = 100   # Maximum current (A)
 
 # Declare variables
-m_h2 = ca.MX.sym('m_h2') # State - Mass of hydrogen
+v_h2 = ca.MX.sym('v_h2') # State - Volume of hydrogen
 i_el = ca.MX.sym('i_el') # Control - Electrical current in electrolyzer
 p_el = ca.MX.sym('p_el') # Control - Electrolyzer state
 time = ca.MX.sym('time') # Time
 
-# Intermediate electrolyzer variables
-i = i_el/A_el # Current density
-ro_b = (0.005139*lambda_b - 0.00326) * ca.exp(1268*(1/303 - 1/t_el)) # Membrane conductivity
-v_el_0 = 1.23 - 0.0009*(t_el-298) + 2.3*R*t_el*ca.log(P_h2**2*P_o2)/(4*F) # Reversible potential of the electrolyzer
-v_etd = (R*t_el/F)*ca.asinh(.5*i/I_ao) + (R*t_el/F)*ca.asinh(.5*i/I_co) + i*delta_b/ro_b # Eletrode overpotential
-v_el_hom_ion = delta_b*i_el/(A_el*ro_b) # Ohmic overvoltage and ionic overpotential
-
-# Intermediate photovoltaic variables
-Vt = K*T_ps/Q
-Iph = (Isc+Kl*(T_ps-Tr))*Irradiation(time)
-Irs = Ior*(T_ps/Tr)** 3*ca.exp(Q*Ego*(1/Tr-1/T_ps)/(K*A))
-
-# Algebraic equations
-f_h2 =  p_el*(N_el*i_el/F)*(11.126/(60*1000)) # Hydrogen production rate (Nm3/min)
-v_el = v_el_0 + v_etd + v_el_hom_ion
-v_ps = (N_ss*Vt*A*(lambertw(ca.exp(1)*(Iph/Irs+1))-1))
-i_ps = N_ps*(Iph-Irs*(ca.exp(v_ps/(N_ss*Vt))-1)) 
+# Models equations
+[f_h2, v_el] = electrolyzer_model(i_el) # Hydrogen production rate (Nm3/min) and eletrolyzer voltage (V)
+f_h2 = p_el*f_h2 # Switching between electrolyzer states
+[i_ps, v_ps] = pv_model(Irradiation(time)) # Power and voltage of the photovoltaic panel (A, V)
+v_h2_dot = thank_model(f_h2, HydrogenDemand(time)/60) # Hydrongen volume rate in the tank (Nm3/min)
 
 # Lagrange cost function
-f_q = (p_el*(I_e_min-i_el))*((N_el*v_el*i_el) - v_ps*i_ps)**2
+f_l = (p_el*(I_e_min-i_el))*((N_el*v_el*i_el) - v_ps*i_ps)**2
 
-# Diferential equations
-m_h2_dot = f_h2 - HydrogenDemand(time)/60
-
-f = ca.Function('f', [m_h2, i_el, p_el, time], [m_h2_dot, f_q], ['x', 'u_i', 'u_p', 't'], ['x_dot', 'L'])
+f = ca.Function('f', [v_h2, i_el, p_el, time], [v_h2_dot, f_l], ['x', 'u_i', 'u_p', 't'], ['x_dot', 'L'])
 
 # Creat NPL problem
 t = np.linspace(0, Tf, num=N, endpoint=True)
