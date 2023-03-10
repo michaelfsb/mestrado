56debf5d62b6912a8b8d01901fc55f6864f38d75
Michael Barbosa <michaelfeliphe@gmail.com>
2023-03-10 11:33:21 -0300

    Remove unused import

--- ocp-2-single-shooting.py
+++ ocp-2-single-shooting.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from models.photovoltaic_panel import pv_model
-from models.electrolyzer import N_el, electrolyzer_model
+from models.electrolyzer import electrolyzer_model
 from models.tank import thank_model
 from models.input_data import Irradiation, HydrogenDemand
 from utils import files
