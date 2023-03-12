3df3852d3023c5a3aca62353edd24e68c986c2c0
Michael Barbosa <michaelfeliphe@gmail.com>
2023-03-12 13:51:59 -0300

    Fix and simulate fuel cell model

--- models/fuel_cell.py
+++ models/fuel_cell.py
@@ -1,21 +1,36 @@
 import casadi as ca
 
+from utils.convesions import h2_flow_converter
+
 def fuel_cell_model(i_fc):
     """ Fuel cell model """	
 
-    T_0 = 296   # Nominal operating temperature (K)
-    A_fc = 61   # Stack area (cm2)
-    P_H2 = 1.25 # H2 partial pressure (atm)
+    # Declare constants
+    F = 96485.33289 # Faraday constant
 
-    # P_O2 need to be a state variable
-    # t_fc nedd to be defined if will be fixed or variable 
-
+    # Declare fuel cell parameters
+    N_fc = 60           # Number of cells
+    T_0 = 296           # Nominal operating temperature (K)
+    A_fc = 61           # Stack area (cm2)
+    P_H2 = 1.25         # H2 partial pressure (atm)
+    U_0 = 1.046         # Open circuit voltage (V)
+    K_1T = 0.003        # Temperature coefficient (V/K)
+    K_1_p = 0.244       # Pressure coefficient (V/atm)s
+    K_1_act = 0.066     # Activation losses coefficient (V/A)
+    K_2_act = 0.013     # Activation losses coefficient (A)
+    R_fc = 0.299        # Resistance (ohm)
+    K_1_conc = 0.028    # Concentration coefficient (V/A)
+    K_2_conc = 9.001    # Concentration coefficient (V/A)
+    t_fc = 296          # Temperature
+    
+    P_O2 = (0.09/12.10)*i_fc + 0.15 # Oxygen partial pressure (atm)
     i_fc_d = i_fc/A_fc # Current density
-    v_fc_0 = 1.046 + 0.003*(t_fc - T_0) + 0.244*(.5*ca.log(P_O2) + ca.log(P_H2))# Open circuit voltage
-    v_act = 0.066*(1 - ca.exp(-i_fc_d/0.013)) # Activation losses
-    v_ohm = 0.299*i_fc # Ohmic losses
-    v_conc = 0.028*i_fc_d**9.001 #
+    v_fc_0 = U_0 + K_1T*(t_fc - T_0) + K_1_p*(.5*ca.log(P_O2) + ca.log(P_H2)) # Open circuit voltage
+    v_act = K_1_act*(1 - ca.exp(-i_fc_d/K_2_act)) # Activation losses
+    v_ohm = R_fc*i_fc_d # Ohmic losses
+    v_conc = K_1_conc*i_fc_d**K_2_conc # Concentration losses
 
-    v_fc = v_fc_0 - v_act - v_ohm - v_conc
+    f_h2_cons = (N_fc*i_fc/(F*1000))*h2_flow_converter("Nl/min") # Hydrogen consumption rate
+    v_fc = N_fc*(v_fc_0 - v_act - v_ohm - v_conc) 
     p_fc = v_fc*i_fc
-    return [v_fc, p_fc]
+    return [f_h2_cons, v_fc, p_fc]
---
+++ models/simulations/fuel_cell.py
@@ -0,0 +1,33 @@
+from matplotlib import pyplot as plt
+from models.fuel_cell import fuel_cell_model
+
+# Simulation of the fuel cell
+current = range(0, 70, 1)
+voltage = []
+power = []
+h2_consumption = []
+for i in range(len(current)):
+    [f_h2, v_fc, p_fc] = fuel_cell_model(current[i])
+    voltage.append(v_fc)
+    power.append(v_fc*current[i])
+    h2_consumption.append(f_h2)
+
+# Plot the results
+# plt.plot(current, h2_consumption, label="H2 consumption")
+# plt.xlabel("Current (A)")
+# plt.ylabel("H2 consumption (Nl/min)")
+# plt.show()
+
+plt.plot(current, voltage, label="Voltage")
+plt.xlabel("Current (A)")
+plt.ylabel("Voltage (V)")
+plt.show()
+
+# plt.plot(current, power, label="Power")
+# plt.xlabel("Current (A)")
+# plt.ylabel("Power (W)")
+# plt.show()
+
+# Generate a Latex plot of the results (cant be used with plt.show() and plt.legend() functions)
+# import tikzplotlib
+# tikzplotlib.save("models/simulations/fuel_cell.tex")
