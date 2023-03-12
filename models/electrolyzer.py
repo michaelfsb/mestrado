4448aa5f025660b1592ce7c1636665527b9cfd3c
Michael Barbosa <michaelfeliphe@gmail.com>
2023-03-12 13:52:17 -0300

    Minor fix electrolyzer

--- models/electrolyzer.py
+++ models/electrolyzer.py
@@ -6,7 +6,7 @@ def electrolyzer_model(i_el):
 
     # Declare constants
     R = 8.314 # Gas constant
-    F = 96485 # Faraday constant
+    F = 96485.33289 # Faraday constant
     
     # Declare electrolyzer parameters
     A_el = 212.5            # Stack area
@@ -26,8 +26,8 @@ def electrolyzer_model(i_el):
     v_etd = (R*t_el/F)*ca.asinh(.5*i_el_d/I_ao) + (R*t_el/F)*ca.asinh(.5*i_el_d/I_co) + i_el_d*delta_b/ro_b # Eletrode overpotential
     v_el_hom_ion = delta_b*i_el/(A_el*ro_b) # Ohmic overvoltage and ionic overpotential
 
-    f_h2 = (N_el*i_el/(F*1000))*h2_flow_converter("Nl/min") # Hydrogen production rate
+    f_h2_prd = (N_el*i_el/(F*1000))*h2_flow_converter("Nl/min") # Hydrogen production rate
     v_el = N_el*(v_el_0 + v_etd + v_el_hom_ion) # Eletrolyzer voltage
     p_el = i_el*v_el # Eletrolyzer consumed power
 
-    return [f_h2, v_el, p_el]
+    return [f_h2_prd, v_el, p_el]
