824fadd220cf9d7c0969f724824de3bb5e77ae3c
Michael Barbosa <michaelfeliphe@gmail.com>
2023-03-25 15:02:48 -0300

    Fix OCP1 HS

--- ocp-1-hs-collocation.py
+++ ocp-1-hs-collocation.py
@@ -11,10 +11,11 @@ from models.electrolyzer import electrolyzer_model
 from models.tank import thank_model
 from models.input_data import Irradiation, HydrogenDemand
 from utils import files
+from utils.interpolations import control_spline
 
 # Preliminaries
 Tf = 1440   # Final time (min)
-N = 15      # Number of control intervals
+N = 38      # Number of control intervals
 
 M_0 = 0.65      # Initial volume of hydrogen (Nm3)
 M_min = 0.6     # Minimum volume of hydrogen (Nm3)
@@ -38,13 +39,13 @@ f_l = (p_el - p_ps)**2
 
 # Creat NPL problem
 f = ca.Function('f', [v_h2, i_el, time], [v_h2_dot, f_l], ['x', 'u', 't'], ['x_dot', 'L'])
-t = np.linspace(0, Tf, num=2*N+1, endpoint=True)
-h = [t[k+1]-t[k] for k in range(t.size-1)]
+t = np.linspace(0, Tf, num=N, endpoint=True)
+h = Tf/N
 
 X = []
 U = []
 
-for k in np.arange(0, N+.5, .5):
+for k in np.arange(0, N-.5, .5):
     X += [ca.MX.sym('X_' + str(k))]
     U += [ca.MX.sym('U_' + str(k))]
 
@@ -52,18 +53,18 @@ L = 0
 g = []
 lbg = []
 ubg = []
-for k in np.arange(0, 2*N, 2):
+for k in np.arange(0, 2*N-2, 2):
     i = int(k/2)
-    f_k_0, w_k_0 = f(X[k], U[k], t[k])
-    f_k_1, w_k_1 = f(X[k+1], U[k+1], t[k+1])
-    f_k_2, w_k_2 = f(X[k+2], U[k+2], t[k+2])
-    L = L + h[i]*(w_k_0 + 4*w_k_1 + w_k_2)/6
+    f_k_0, w_k_0 = f(X[k], U[k], t[i])
+    f_k_1, w_k_1 = f(X[k+1], U[k+1], t[i]+h/2)
+    f_k_2, w_k_2 = f(X[k+2], U[k+2], t[i+1])
+    L = L + h*(w_k_0 + 4*w_k_1 + w_k_2)/6
 
     # Add equality constraint
-    g +=  [h[i]*(f_k_0 + 4*f_k_1 + f_k_2)/6 + X[k] - X[k+2]]
+    g +=  [X[k+2] - X[k] - h*(f_k_0 + 4*f_k_1 + f_k_2)/6]
     lbg += [0]
     ubg += [0]
-    g +=  [.5*(X[k+2] + X[k]) + h[i]*(f_k_0 - f_k_1)/8 - X[k+1]]
+    g +=  [X[k+1] - (X[k+2] + X[k])/2 - h*(f_k_0 - f_k_2)/8]
     lbg += [0]
     ubg += [0]
 
@@ -76,7 +77,7 @@ ubw = []
 x_plot = []
 u_plot = []
 
-for k in range(2*N+1):
+for k in range(2*N-1):
     # New NLP variable for the control
     w += [U[k]]
     lbw += [I_e_min]
@@ -127,7 +128,10 @@ x_opt, u_opt = trajectories(sol['x'])
 x_opt = x_opt.full().flatten()
 u_opt = u_opt.full().flatten()
 
+[taa, uaa] = control_spline(t, u_opt)
+
 # Plot results
+t = np.linspace(0, Tf, num=2*N-1, endpoint=True)
 f_x_opt = interpolate.interp1d(t, x_opt, kind=3) # k=3 is a cubic spline
 f_u_opt = interpolate.interp1d(t, u_opt, kind=2) # k=2 is a quadratic spline
 t_new = np.arange(0, Tf, 0.1)
@@ -135,7 +139,8 @@ t_new = np.arange(0, Tf, 0.1)
 fig, axs = plt.subplots(2,1)
 fig.suptitle('Simulation Results: ' + optimzation_status + '\nCost: ' + str(sol['f']) + ' (W)')
 
-axs[0].plot(t/60, u_opt, '.r', t_new/60, f_u_opt(t_new), '-b')
+#axs[0].plot(t/60, u_opt, '.r')
+axs[0].plot(t/60, u_opt, '.r', np.array(taa)/60, np.array(uaa), '-b')
 axs[0].set_ylabel('Electrolyzer current [A]')
 axs[0].grid(axis='both',linestyle='-.')
 axs[0].set_xticks(np.arange(0, 26, 2))
@@ -149,14 +154,14 @@ axs[1].set_xticks(np.arange(0, 26, 2))
 plt.savefig(files.get_plot_file_name(__file__), bbox_inches='tight', dpi=300)
 
 # Evaluate the error
-te = np.linspace(0, Tf, num=2*N, endpoint=True)
+te = np.linspace(0, Tf, num=10*N, endpoint=True)
 f_interpolated = interpolate.interp1d(t, f(x_opt, u_opt, t)[0].full().flatten(), kind=2)
 error = f(f_x_opt(te), f_u_opt(te), te)[0] - f_interpolated(te)
 
 # Plot error
 fig2 = plt.figure(2)
 fig2.suptitle('Erro in differential equations')
-plt.plot(te/60, error, '-', t/60, np.zeros(len(t)), '.r')
+plt.plot(te/60, error, '.', t/60, np.zeros(len(t)), '.r')
 plt.ylabel('Error')
 plt.xlabel('Time [h]')
 plt.grid(axis='both',linestyle='-.')
