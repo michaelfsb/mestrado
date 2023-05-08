705d1149910182b25790c2810178b644cb130f35
Michael Barbosa <michaelfeliphe@gmail.com>
2023-05-07 22:21:18 -0300

    Creat problem 3

--- ocp-3.py
+++ ocp-3.py
@@ -1,11 +1,11 @@
-import casadi as ca
-from ocp.opdas import OptimalControlProblem, Time, VariableList
+# Imports
+from ocp.opdas import OptimalControlProblem, Time, VariableList, Phase
 from models.electrolyzer import electrolyzer_model
 from models.input_data import HydrogenDemand, Irradiation
 from models.photovoltaic_panel import pv_model
 from models.tank import thank_model
 
-t = Time(initial=0, final=1440, nGrid=100)
+t = Time(initial=0, final=1440, nGridPerPhase=6)
 
 states = VariableList()
 states.add(name='v_h2', min=0.6, max=2.5)
@@ -18,15 +18,22 @@ problem = OptimalControlProblem(name='ocp-1', controls=controls, states=states, 
 # Models equations
 [f_h2, v_el, p_el] = electrolyzer_model(controls['i_el'])
 [i_ps, v_ps, p_ps] = pv_model(Irradiation(t.value)) 
-v_h2_dot = thank_model(ca.if_else(controls['i_el']>20, f_h2, 0), HydrogenDemand(t.value))
+v_h2_dot = thank_model(f_h2, HydrogenDemand(t.value)) 
+phase_on = Phase(name='on', model=v_h2_dot)
+
+v_h2_dot_off = thank_model(0, HydrogenDemand(t.value)) 
+phase_off = Phase(name='off', model=v_h2_dot_off)
+
+problem.set_phases([phase_off, phase_on, phase_off])
 
 # Lagrange cost function
 f_l = (p_el - p_ps)**2
 
-problem.set_dynamic(v_h2_dot)
 problem.set_langrange_cost(f_l)
 
-problem.set_guess(control=[30], state=[0.65])
+problem.set_langrange_cost(f_l)
+
+problem.set_guess(control=[30, 1], state=[2.5])
 
 problem.solve()
 
